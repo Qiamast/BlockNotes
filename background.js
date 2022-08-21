4b7fa22af2cdd27c673bdf9655340f2c690dd5bb
Mahdi Qiamast <78082316+Qiamast@users.noreply.github.com>
2022-08-21 16:03:20 +0200

    make clean

--- background.js
+++
@@ -1,7 +0,0 @@
-
-let user_note = 'Hello i am Mahdi';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ user_note });
-  console.log('new log >> ', `NOTE: ${user_note}`);
-});
